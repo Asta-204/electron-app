2e88e59ac7070c7abf7193c1596cccd08a92fa7e
Asta-204 <kekeli.akagbor@gmail.com>
2025-06-22 07:22:54 +0000

    upd

--- src/components/SignIn.tsx
+++ src/components/SignIn.tsx
@@ -1,42 +1,64 @@
-import { AppProvider } from '@toolpad/core/AppProvider';
-import { SignInPage, type AuthProvider, type AuthResponse } from '@toolpad/core/SignInPage';
-import { useTheme } from '@mui/material/styles';
+"use client"
 
-const providers = [{ id: 'credentials', name: 'Credentials' }];
-// preview-start
-const BRANDING = {
-  logo: (
-    <img
-      src="https://mui.com/static/logo.svg"
-      alt="MUI logo"
-      style={{ height: 24 }}
-    />
-  ),
-  title: 'MUI',
-};
-// preview-end
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { z } from "zod"
 
-const signIn = async (provider: AuthProvider): Promise<AuthResponse> => {
-const promise: Promise<AuthResponse> = new Promise((resolve) => {
-    setTimeout(() => {
-      console.log(`Sign in with ${provider.id}`);
-      resolve({});
-    }, 500);
-  });
-  return promise;
-};
+import { Button } from "@/components/ui/button"
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
 
-export default function BrandingSignInPage() {
-  const theme = useTheme();
+const formSchema = z.object({
+  username: z.string().min(2, {
+    message: "Username must be at least 2 characters.",
+  }),
+})
+
+export default function ProfileForm() {
+   // 1. Define your form.
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      username: "",
+    },
+  })
+ 
+  // 2. Define a submit handler.
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values)
+  }
+
   return (
-    // preview-start
-    <AppProvider branding={BRANDING} theme={theme}>
-      <SignInPage
-        signIn={signIn}
-        providers={providers}
-        slotProps={{ emailField: { autoFocus: false }, form: { noValidate: true } }}
-      />
-    </AppProvider>
-    // preview-end
-  );
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        <FormField
+          control={form.control}
+          name="username"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Username</FormLabel>
+              <FormControl>
+                 <Input typlaceholder="shadcn" {...field} />
+              </FormControl>
+              <FormDescription>
+                This is your public display name.
+                    </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit">Submit</Button>
+      </form>
+    </Form>
+  )
 }
